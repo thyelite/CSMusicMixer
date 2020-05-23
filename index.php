c3ceb7317d14a933b7c4761eef3aed77c4f496df
thyelite <65800856+thyelite@users.noreply.github.com>
2020-05-22 23:32:15 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,222 @@
+<html>
+	<head>
+		<title>Music Mixer</title>
+
+		<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+		<meta content="utf-8" http-equiv="encoding">
+
+		<link rel="stylesheet" type="text/css" href="assets/css/fonts.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+
+		<script src="assets/js/soundmanager2.js" type="text/javascript"></script>
+
+	</head>
+
+	<body>
+
+
+		<div class="stage" id="load">
+
+			<div id="loadbox">
+			    <div id="loading_bar">
+			        <img src="assets/images/7.svg">
+		        </div>
+				<span id="loadinfo"></span>
+
+			</div>
+		</div>
+
+		<div class="stage" id="main">
+				<div id="arrow">
+			        <img src="assets/images/arrow.gif">
+		        </div>
+		    <div id="topright_bit">
+			    <img src="assets/images/topright.png">
+		    </div>
+		    		    <div id="ingredients_bit">
+			    <img src="assets/images/genre.svg">
+		    </div>
+			<div id="right_bar">
+
+				<ul>
+					<li><a href="#" onclick="Mixer.setPack('Country');" class="Country_class">Country</a></li>
+					<li><a href="#" onclick="Mixer.setPack('Dance');" class="Dance_class">Dance</a></li>
+					<li><a href="#" onclick="Mixer.setPack('Hip-Hop');" class="Hip-Hop_class">Hip-Hop</a></li>
+					<li><a href="#" onclick="Mixer.setPack('Latin');" class="Latin_class">Latin</a></li>
+					<li><a href="#" onclick="Mixer.setPack('Pop');" class="Pop_class">Pop</a></li>
+					<li><a href="#" onclick="Mixer.setPack('Rock');" class="Rock_class">Rock</a></li>
+				</ul>			    				
+				<div id="equalizer">
+				                    <img src="assets/images/4.svg" height="0" width="0">
+			    			        <img src="assets/images/button1_hover.gif" height="0" width="0">
+			    			        <img src="assets/images/play_i.svg" height="0" width="0">
+			    			        <img src="assets/images/hover_i.svg" height="0" width="0">
+			    			        <img src="assets/images/clear_i.svg" height="0" width="0">
+			    			        <img src="assets/images/startnewmix_i.svg" height="0" width="0">
+                                    <img src="assets/images/save_i.svg" height="0" width="0">
+		        </div>
+
+				<a id="huge-button" href="#" onclick="openMixer()"><p id="startmixing"></p></a>
+			</div>
+			<div id="fuck"><p id="storedTracksList" class="no_overflow">You have no stored tracks!</p></div>
+
+<div id="import-button">				<a  href="#" onclick="showImport()"><p id="startmixing">IMPORT</p></a></div>
+
+
+			<div id="welcome_text">
+			    <div id="numbers">1</div><div id="numbers2">2</div>
+			    <h3>INSTRUCTIONS</h3>
+				<h1 id="step1"><em>CHOOSE A GENRE</em></h1>
+				<p>
+					Select an item from the genre list. You have your choice from Country to Hip-Hop. The genre you choose will give you a pre mix template to start you on your journey.<br><br>
+				</p>
+				<h2 id="step2"><em>START MIXING</em></h2>
+				<p>
+					Click on the "START MIXING" button on the bottom right, or double click on a genre mix to begin.
+				</p>
+
+			</div>
+			
+			<div id="bottom_bit">
+			    <img src="assets/images/bottombit.png">
+		    </div>
+		    
+		    
+		    <div id="demo_bit">
+			    <img src="assets/images/createyourdemo2.svg">
+		    </div>
+
+		
+		
+		
+		</div>
+
+				
+		<div class="stage" id="mixer">
+		    <div id="help"> 
+		 <a href="javascript:;" onclick="Mixer.help();"><img src="assets/images/15.svg"></a>
+		    </div>
+		    <div id="demo_bit">
+			    <img src="assets/images/createyourdemo2.svg">
+		    </div>
+							<div id="doubleclick"> 
+		 <img src="assets/images/14.svg">
+		 </div>
+			<div id="backmixer">
+				<img src="assets/images/backmixah.svg" width="750">
+			</div>
+		  <div id="scrollahback1">
+		      <img src="assets/images/scrollahback.svg"><br>
+		  </div>
+		  		  <div id="scrollahback2">
+		      <img src="assets/images/scrollahback.svg"><br>
+		  </div>
+		  		  <div id="scrollahback3">
+		      <img src="assets/images/scrollahback.svg"><br>
+		  </div>
+		  		  <div id="scrollahback4">
+		      <img src="assets/images/scrollahback.svg"><br>
+		  </div>
+		  		  <div id="scrollahback5">
+		      <img src="assets/images/scrollahback.svg"><br>
+		  </div>
+		  		  <div id="scrollahback6">
+		      <img src="assets/images/scrollahback.svg"><br>
+		  </div>
+		  	    <div id="eqbar"><img src="assets/images/eqbar.svg" width="125%"></div>
+			<div id="button-suite">
+		
+				<a id="playButton" class="s_stopped" href="javascript:;" onclick="Mixer.playStopButton();" state="stopped"></a>
+				<a id="clearButton" href="javascript:;" onclick="Mixer.clearAll();"></a>
+				<a id="saveButton" href="javascript:;" onclick="Mixer.save();"></a>
+				<a id="startmixButton" href="javascript:;" onclick="Mixer.save();"></a>
+				<a id="smallButton" href="javascript:;" onclick="Mixer.share();">SHARE</a>
+				<a id="smallButton" href="javascript:;" onclick="showStage('main');">BACK</a>
+				<img src="assets/images/102.svg">&nbsp;
+				<span id="timeline-seconds">0s</span>
+			</div>
+
+			<div id="rows-collection"></div>
+			<div id="timeline-container" style="position: absolute; top: 86.5px; left: 211.5px;">
+			    <div id="dot"> <img src="assets/images/dot.svg" height="10px"></div>
+			    <div id="bar">
+			<img src="assets/images/108.svg" height="278px">
+
+</div>
+				<br>
+				
+
+			</div>
+
+    <div id="packs-area">
+        <div id="packs-list">
+
+    	</div>
+
+
+
+<div id="container">
+				<div id="packs-data">
+
+				</div>
+</div>
+			</div>
+			    <div id="buttonleft"><button id="slideLeft" type="image" src="assets/images/left.svg">&nbsp;</button></div>
+   <div id="buttonright"> <button id="slideRight" type="image" src="assets/images/right.svg">&nbsp;</button></div>
+		</div>
+
+		<script>
+			var v = 1.4;
+
+			var require = {
+				deps: [		"packs.js?"+new Date(),
+							"assets/js/PrototypeTools.js?"+v,
+							"assets/js/LZMA.js?"+v,
+							"assets/js/Base64.js?"+v,
+							"assets/js/Txt.js?"+v,
+							"assets/js/StepSequence.js?"+v,
+							"assets/js/Mixer.js?"+v,
+							"assets/js/Interface.js?"+v,
+							"assets/js/Packs.js?"+v,
+							"assets/js/main.js?"+v,
+						],
+				callback: function(module1, module2) {
+					window.addEventListener("load", new function() {
+						loadingStatus("Preparing SoundManager");
+
+						showStage("load");
+						soundManager.setup({
+							url: 'assets/swf/',
+							onready: function() {
+								loadingStatus("Loading ...");
+
+								window.lzmalib = new LZMA("assets/js/lzma_worker.js");
+
+								InstalledPacks.forEach(function (a) {
+									Packs.prepared++;
+									Packs.loadPack("packs/"+a+"/"+a+".js?"+v);
+								});
+							},
+							ontimeout: function() {
+								loadingStatus("Error! Could not load SoundManager Library");
+							}
+
+						});
+
+					});
+				}
+			};
+			    const buttonRight = document.getElementById('slideRight');
+    const buttonLeft = document.getElementById('slideLeft');
+
+    buttonRight.onclick = function () {
+      document.getElementById('container').scrollLeft += 30;
+    };
+    buttonLeft.onclick = function () {
+      document.getElementById('container').scrollLeft -= 30;
+    };
+		</script>
+
+		<script src="assets/js/require.js"></script>
+	</body>
+</html>
